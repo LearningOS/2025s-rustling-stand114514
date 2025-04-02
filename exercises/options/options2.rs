f29d15bf1a20641a232538b57c746534063f8992
Stand <1687771866@qq.com>
2025-04-02 13:32:39 +0800

    update

--- exercises/options/options2.rs
+++ exercises/options/options2.rs
@@ -3,8 +3,6 @@
 // Execute `rustlings hint options2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 #[cfg(test)]
 mod tests {
     #[test]
@@ -12,9 +10,9 @@ mod tests {
         let target = "rustlings";
         let optional_target = Some(target);
 
-        // TODO: Make this an if let statement whose value is "Some" type
-        word = optional_target {
-            assert_eq!(word, target);
+        // TODO: 将其设置为值为“Some”类型的if let语句
+        if let Some(word) = optional_target {
+            assert_eq!(word, target)
         }
     }
 
@@ -29,12 +27,13 @@ mod tests {
 
         let mut cursor = range;
 
-        // TODO: make this a while let statement - remember that vector.pop also
-        // adds another layer of Option<T>. You can stack `Option<T>`s into
-        // while let and if let.
-        integer = optional_integers.pop() {
-            assert_eq!(integer, cursor);
-            cursor -= 1;
+        // TODO: 将此转换为 `while let` 语句 - 记住 `vector.pop` 也会增加一层 `Option<T>`。
+        // 你可以将多个 `Option<T>` 嵌套到 `while let` 和 `if let` 中。
+        while let Some(integer) = optional_integers.pop() {
+            if let Some(integer) = integer {
+                assert_eq!(integer, cursor);
+                cursor -= 1;
+            }
         }
 
         assert_eq!(cursor, 0);
